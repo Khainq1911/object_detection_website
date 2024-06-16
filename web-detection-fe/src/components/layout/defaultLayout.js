8c6d382691a31820f50ae9e6797c14b7b33099a7
khainq1911 <khaitom1911@gmail.com>
2024-06-17 00:07:45 +0700

    update layout

--- web-detection-fe/src/components/layout/defaultLayout.js
+++ web-detection-fe/src/components/layout/defaultLayout.js
@@ -1,13 +1,11 @@
 import Header from "~/components/Header";
 import Footer from "~/components/Footer";
 
-function Defaultlayout() {
+function Defaultlayout({ children }) {
   return (
     <div>
       <Header />
-      <div className="content">
-        <h1>body</h1>
-      </div>
+      <div className="content">{children}</div>
       <Footer />
     </div>
   );
