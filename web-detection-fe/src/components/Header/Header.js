493944c2210d4fc93c8f60ea5ed581936b394beb
khainq1911 <khaitom1911@gmail.com>
2024-07-10 00:20:13 +0700

    header update

--- web-detection-fe/src/components/Header/Header.js
+++ web-detection-fe/src/components/Header/Header.js
@@ -1,8 +1,47 @@
-function Header(){
-    return (
-        <div>
-            <h1>header</h1>
-        </div>
-    )
+import classNames from "classnames/bind";
+import styles from "./Header.module.scss";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faBars, faChartPie, faHouse } from "@fortawesome/free-solid-svg-icons";
+import { useState } from "react";
+
+const url = "http://localhost:1911/";
+const cx = classNames.bind(styles);
+
+function Header() {
+  const [activeMenu, setActiveMenu] = useState(false);
+
+  const handleOpenMenu = () => {
+    setActiveMenu(!activeMenu);
+  };
+  return (
+    <div className={cx("header_container")}>
+      <nav className={cx("nav_container")}>
+        <ul>
+          <li>
+            <a href={url}>
+              <FontAwesomeIcon icon={faHouse} className={cx("icon")} />
+              Home
+            </a>
+          </li>
+          <li>
+            <a href={url + `chart`}>
+              <FontAwesomeIcon icon={faChartPie} className={cx("icon")} />
+              Chart
+            </a>
+          </li>
+        </ul>
+      </nav>
+      <div className={cx("header_menu_container")} onClick={handleOpenMenu}>
+        <FontAwesomeIcon icon={faBars} />
+        {activeMenu && (
+          <div className={cx("menu")}>
+            <p>Filter Data</p>
+            <p>Account</p>
+            <p>Logout</p>
+          </div>
+        )}
+      </div>
+    </div>
+  );
 }
-export default Header
+export default Header;
